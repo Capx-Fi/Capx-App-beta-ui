7f6fd810e9d1eb9c5451cd15a1ae54006ca8c695
Parvez Ansari <parvez.ansari@capx.global>
2022-12-08 14:29:51 +0530

    miner changes added

--- src/pages/home/components/OldTasks/OldTasks.js
+++ src/pages/home/components/OldTasks/OldTasks.js
@@ -7,7 +7,7 @@ const handleDragStart = (e) => e.preventDefault();
 const OldTasks = ({ quests }) => {
   return (
     <div className="oldtasks">
-      <div className="oldtasks-wrapper flex flex-col gap-12 md:flex-col">
+      <div className="oldtasks-wrapper flex flex-col gap-8 md:flex-col">
         {quests.map((data, ind) => {
           return (
             <div
@@ -19,11 +19,15 @@ const OldTasks = ({ quests }) => {
               <div className="oldtask-row-1 flex flex-col gap-3 md:flex-row w-full justify-between">
                 <div className="oldtask-chip flex flex-row py-1 px-2 gap-1 items-center rounded-xl border-2">
                   <img src={CardCoinIcon} className="w-5"></img>
-                  <p className="fs-13">{data.taskreward + " xCapx"}</p>
+                  <p className="fs-12 font-black">
+                    {data.taskreward + " xCapx"}
+                  </p>
                 </div>
 
                 <div className="oldtask-chip flex flex-row p-2 gap-1 items-center rounded-xl border-2">
-                  <p className="fs-13">Completed by {data.completed_by}</p>
+                  <p className="fs-12  font-black">
+                    Completed by {data.completed_by}
+                  </p>
                   <img src={PeopleIcon} className="w-5"></img>
                 </div>
               </div>

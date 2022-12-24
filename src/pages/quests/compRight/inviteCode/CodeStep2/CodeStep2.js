17c4f6e1f93ab9804a13f03bf3b359cee387add5
Parvez Ansari <parvez.ansari@capx.global>
2022-12-24 21:25:00 +0530

    invite code share modal added

--- src/pages/quests/compRight/inviteCode/CodeStep2/CodeStep2.js
+++ src/pages/quests/compRight/inviteCode/CodeStep2/CodeStep2.js
@@ -8,78 +8,105 @@ import {
   TwitterIcon,
   WhatsappIcon,
 } from "../../../../../assets/svg";
+import ShareModal from "../shareModal/ShareModal";
 
 const Codestep2 = ({ actionData }) => {
   const userInviteCode = useSelector((state) => state.user.invite_code);
-
   const [showSharingOptions, setShowSharingOptions] = useState(false);
-  return (
-    <div className="codestep2 flex flex-col gap-3">
-      <p className="codestep2-title action-heading   ">
-        Action #1 : Share Invite Code
-      </p>
-      <div className="codestep2-wrapper mb-3">
-        <div className="codestep2-placeholder px-5 py-4 rounded-2xl text-cgreen-700 flex justify-between">
-          <span> {userInviteCode}</span>
-          <button
-            className="flex items-center justify-center"
-            onClick={() => {
-              navigator.clipboard.writeText(userInviteCode);
-            }}
-          >
-            <img src={ContentCopySvg} alt="copy" />
-          </button>
-        </div>
-      </div>
-      {!showSharingOptions && actionData?.action_order_status !== 'COMPLETED' && (
-        <button
-          className="bg-gredient-2 action-btn self-stretch flex justify-center items-center p-3 rounded-2xl "
-          onClick={() => {
-            setShowSharingOptions(true);
-          }}
-        >
-          Invite Your Frens
-          <HiArrowRight className="text-xl ml-4" />
-        </button>
-      )}
+  const [openShareModal, setOpenShareModal] = useState(false);
+  const [shareingPlatform, setShareingPlatform] = useState("");
 
-      {showSharingOptions && actionData?.is_claimed === false  && (
-        <div className="codestep2-congrats w-full flex flex-col gap-6">
-          <div className="codestep2-congrats-1 flex flex-col gap-4">
-            <div className="social-icons flex flex-col gap-3 w-full">
-              <button className=" flex w-full rounded-xl border-2 border-primary-200">
-                <img src={TwitterIcon} alt="google" />
-                <span className="font-semibold fs-15 ml-4 text-cgreen-700">
-                  Share on Twitter
-                </span>
-              </button>
-              <button className=" flex w-full rounded-xl border-2 border-primary-200">
-                <img src={InstagramIcon} alt="google" />
-                <span className="font-semibold fs-15 ml-4 text-cgreen-700">
-                  Share on Instagram
-                </span>
-              </button>
-              <button className=" flex w-full rounded-xl border-2 border-primary-200">
-                <img src={WhatsappIcon} alt="google" />
-                <span className="font-semibold fs-15 ml-4 text-cgreen-700">
-                  Share on Whatsapp
-                </span>
-              </button>
-            </div>
+  const handleshareModal = () => {
+    setOpenShareModal(false);
+    navigator.clipboard.writeText(userInviteCode);
+  };
+
+  return (
+    <>
+      <div className="codestep2 flex flex-col gap-3">
+        <p className="codestep2-title action-heading   ">
+          Action #1 : Share Invite Code
+        </p>
+        <div className="codestep2-wrapper mb-3">
+          <div className="codestep2-placeholder px-5 py-4 rounded-2xl text-cgreen-700 flex justify-between">
+            <span> {userInviteCode}</span>
             <button
-              className="bg-gredient-2 action-btn self-stretch flex justify-center items-center p-3 rounded-2xl "
-              onClick={(e) =>
-                actionData.handleCompleteAction(e, { type: "inviteCode" })
-              }
+              className="flex items-center justify-center"
+              onClick={() => {
+                navigator.clipboard.writeText(userInviteCode);
+              }}
             >
-              Complete Quest
-              <HiArrowRight className="text-xl ml-4" />
+              <img src={ContentCopySvg} alt="copy" />
             </button>
           </div>
         </div>
-      )}
-      {
-        actionData?.is_claimed === true  && (
+        {!showSharingOptions &&
+          actionData?.action_order_status !== "COMPLETED" && (
+            <button
+              className="bg-gredient-2 action-btn self-stretch flex justify-center items-center p-3 rounded-2xl "
+              onClick={() => {
+                setShowSharingOptions(true);
+              }}
+            >
+              Invite Your Frens
+              <HiArrowRight className="text-xl ml-4" />
+            </button>
+          )}
+
+        {showSharingOptions && actionData?.is_claimed === false && (
+          <div className="codestep2-congrats w-full flex flex-col gap-6">
+            <div className="codestep2-congrats-1 flex flex-col gap-4">
+              <div className="social-icons flex flex-col gap-3 w-full">
+                <button
+                  onClick={() => {
+                    setShareingPlatform("Twitter");
+                    setOpenShareModal(true);
+                  }}
+                  className=" flex w-full rounded-xl border-2 border-primary-200"
+                >
+                  <img src={TwitterIcon} alt="google" />
+                  <span className="font-semibold fs-15 ml-4 text-cgreen-700">
+                    Share on Twitter
+                  </span>
+                </button>
+                <button
+                  onClick={() => {
+                    setShareingPlatform("Instagram");
+                    setOpenShareModal(true);
+                  }}
+                  className=" flex w-full rounded-xl border-2 border-primary-200"
+                >
+                  <img src={InstagramIcon} alt="google" />
+                  <span className="font-semibold fs-15 ml-4 text-cgreen-700">
+                    Share on Instagram
+                  </span>
+                </button>
+                <button
+                  onClick={() => {
+                    setShareingPlatform("Whatsapp");
+                    setOpenShareModal(true);
+                  }}
+                  className=" flex w-full rounded-xl border-2 border-primary-200"
+                >
+                  <img src={WhatsappIcon} alt="google" />
+                  <span className="font-semibold fs-15 ml-4 text-cgreen-700">
+                    Share on Whatsapp
+                  </span>
+                </button>
+              </div>
+              <button
+                className="bg-gredient-2 action-btn self-stretch flex justify-center items-center p-3 rounded-2xl "
+                onClick={(e) =>
+                  actionData.handleCompleteAction(e, { type: "inviteCode" })
+                }
+              >
+                Complete Quest
+                <HiArrowRight className="text-xl ml-4" />
+              </button>
+            </div>
+          </div>
+        )}
+        {actionData?.is_claimed === true && (
           <div className="codestep2-congrats w-full flex flex-col gap-6">
             <div className="codestep2-congrats-1 flex flex-col gap-4">
               <div className="social-icons flex flex-col gap-3 w-full">
@@ -102,12 +129,17 @@ const Codestep2 = ({ actionData }) => {
                   </span>
                 </button>
               </div>
-              
             </div>
           </div>
-        )
-      }
-    </div>
+        )}
+      </div>
+      <ShareModal
+        open={openShareModal}
+        platform={shareingPlatform}
+        handleClose={handleshareModal}
+        inviteCode={userInviteCode}
+      />
+    </>
   );
 };
 

f91fd12b8bd53e73d95ca2222ca71dbc58463e35
Parvez Ansari <parvez.ansari@capx.global>
2022-12-26 20:19:44 +0530

    quest profile component added

--- src/pages/quests/compRight/buildProfile/profile/Profile.js
+++ src/pages/quests/compRight/buildProfile/profile/Profile.js
@@ -1,28 +1,35 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { FullName } from "../../../../../assets/images/profile";
 import {
   DailyQuestsIcon,
+  DiscordIcon,
   InputCheckSvg,
   ProfileoutlineIconSvg,
+  TwitterContainedIcon,
 } from "../../../../../assets/svg";
 
 const Profile = () => {
+  const userData = useSelector((state) => state.user);
+
   return (
-    <div className="comp-profile flex flex-col">
-      <div className="img-box md:self-start rounded-full ">
+    <div className="comp-profile flex flex-col gap-8">
+      <div className="img-box md:self-start self-center rounded-full ">
+        {/* uncomment this image tag put profile image data here  */}
         <img
-          className="rounded-full object-contain w-full"
-          src={ProfileoutlineIconSvg}
+          className="rounded-full w-full"
+          src={userData?.image_url}
           alt="Profile"
         />
       </div>
 
-      <div className="name ">
+      <div className="name flex flex-col gap-3">
         <div className="heading flex items-center">
           <img src={FullName} alt="" />
           <span className="ml-3">Full Name</span>
         </div>
         <div className="field-box flex jutify-between">
+          <span className="grow capitalize">{userData?.name}</span>
           <img src={InputCheckSvg} alt="check" />
         </div>
       </div>
@@ -33,11 +40,15 @@ const Profile = () => {
           <span className="ml-3">Socials</span>
         </div>
         <div className="field-box flex jutify-between">
+          <img src={TwitterContainedIcon} alt="twitter" />
+          <span className="grow ml-2">{userData?.socials.twitter_id}</span>
           <img src={InputCheckSvg} alt="check" />
         </div>
-        <div className="field-box flex jutify-between">
+        {/* <div className="field-box flex jutify-between">
+          <img src={DiscordIcon} alt="twitter" />
+          <span className="grow ml-2">Dummy data</span>
           <img src={InputCheckSvg} alt="check" />
-        </div>
+        </div> */}
       </div>
     </div>
   );

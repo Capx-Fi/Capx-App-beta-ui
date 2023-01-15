51eba6b2ed7e8b3d349380c8ec443c266b4c08e7
parvez-capx <115526513+parvez-capx@users.noreply.github.com>
2023-01-15 10:00:42 +0530

    skeleton loader added in home, leaderboardand and wallet screens

--- src/pages/leaderboard/components/skeleton/Skeleton.js
+++ src/pages/leaderboard/components/skeleton/Skeleton.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { LeaderboardSplash } from "../../../../assets/images";
 
 const Skeleton = () => {
   return (
-    <div className="leaderboard-skeleton animate-pulse">
+    <div className="leaderboard-skeleton flex flex-col gap-6 animate-pulse">
       <div className="flex md:flex-row flex-col gap-6">
         {Array(3)
           .fill("")
@@ -20,6 +21,59 @@ const Skeleton = () => {
               </div>
             );
           })}
+      </div>
+      <div className="flex">
+        <div className="top-users-table">
+          <div className="table-wrapper">
+            <table className="w-full">
+              <thead>
+                <tr>
+                  <th className="table-heading">
+                    <span className="md:inline-block hidden">User</span>{" "}
+                    <span>Rank</span>
+                  </th>
+                  <th className="table-heading">User Name</th>
+                  <th className="table-heading">No. of Quests Completed</th>
+                  <th className="table-heading">Total Earnings</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Array(7)
+                  .fill("")
+                  .map((user, ind) => {
+                    console.log(user);
+                    return (
+                      <tr key={user.username + ind}>
+                        <td className="table-data">
+                          <div className="flex justify-start">
+                            <div className="skeleton-bg rounded position" />
+                          </div>
+                        </td>
+                        <td className="table-data text-center">
+                          <div className="flex justify-center">
+                            <div className="skeleton-bg rounded name" />
+                          </div>
+                        </td>
+                        <td className="table-data text-center">
+                          <div className="flex justify-center">
+                            <div className="skeleton-bg rounded quests" />
+                          </div>
+                        </td>
+                        <td className="table-data text-center">
+                          <div className="flex justify-center">
+                            <div className="skeleton-bg rounded earned" />
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="splash-img grow md:flex justify-center items-center hidden ">
+          <img src={LeaderboardSplash} alt="Trophy" />
+        </div>
       </div>
     </div>
   );

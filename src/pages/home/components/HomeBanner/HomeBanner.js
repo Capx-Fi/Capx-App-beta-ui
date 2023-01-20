4fd8c47824e0c2a1fb1d7c27e48ceb2af003bfb0
parvez-capx <115526513+parvez-capx@users.noreply.github.com>
2023-01-20 15:32:41 +0530

    home page new banner design updated

--- src/pages/home/components/HomeBanner/HomeBanner.js
+++ src/pages/home/components/HomeBanner/HomeBanner.js
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { ImArrowRight2 } from "react-icons/im";
 
 function HomeBanner() {
   const sliderSettings = {
@@ -20,7 +21,7 @@ function HomeBanner() {
     <div className="home-banner  rounded-2xl md:rounded-3xl ">
       <div className="w-full">
         <Slider {...sliderSettings}>
-          <div className="slide slider-1 flex flex-row w-full rounded-2xl md:rounded-3xl text-cgreen-100">
+          {/* <div className="slide slider-1 flex flex-row w-full rounded-2xl md:rounded-3xl text-cgreen-100">
             <div className="flex flex-col items-start  gap-6">
               <h2 className="gredient-text-2">
                 Explore the World of Web3
@@ -43,18 +44,6 @@ function HomeBanner() {
               </h2>
             </div>
           </div>
-          {/* <div className="slide slider-3 flex flex-row w-full rounded-2xl md:rounded-3xl text-cgreen-100">
-            <div className="flex flex-col items-start  gap-6">
-              <h2 className="gredient-text-2">
-                Reminder! Your
-                <br className="block md:hidden" /> Unclaimed
-                <br className="hidden md:block" />
-                xCapx
-                <br className="block md:hidden" /> Tokens will expire
-                <br className="block md:hidden" /> soon
-              </h2>
-            </div>
-          </div> */}
           <div className="slide slider-4 flex flex-row w-full rounded-2xl md:rounded-3xl text-cgreen-100">
             <div className="flex flex-col items-start  gap-6">
               <h2 className="gredient-text-2">
@@ -62,6 +51,47 @@ function HomeBanner() {
                 <br /> Earn upto 100xCapx
                 <br /> Coming Soon!
               </h2>
+            </div>
+          </div> */}
+          <div className="slide slider-1 flex flex-row w-full rounded-2xl md:rounded-3xl text-cgreen-100">
+            <div className="flex flex-col items-start  gap-6">
+              <h2 className="gredient-text-2">
+                Come join hundreds of <br className="md:hidden block" /> others
+                in the
+                <br className="hidden md:block" /> gang-gang
+                <br className="md:hidden block" /> contest to earn big!
+              </h2>
+              <button className="banner-btn contained-effect flex items-center bg-gredient-2 hidden md:flex justify-between">
+                <span className="fs-16 font-black mr-3">Participate</span>
+                <ImArrowRight2 />
+              </button>
+            </div>
+          </div>
+          <div className="slide slider-2 flex flex-row w-full rounded-2xl md:rounded-3xl text-cgreen-100">
+            <div className="flex flex-col items-start  gap-6">
+              <h2 className="gredient-text-2">
+                Tweet a little, flex a lot,
+                <br /> to earn 5 xCapx tokens
+              </h2>
+              <button className="banner-btn contained-effect flex items-center bg-gredient-2 hidden md:flex justify-between">
+                <span className="fs-16 font-black mr-3">Begin Quest</span>
+                <ImArrowRight2 />
+              </button>
+            </div>
+          </div>
+          <div className="slide slider-3 flex flex-row w-full rounded-2xl md:rounded-3xl text-cgreen-100">
+            <div className="flex flex-col items-start  gap-6">
+              <h2 className="gredient-text-2">
+                Capx App is your one
+                <br className="md:hidden block" /> place to learn,
+                <br className="md:block hidden" /> explore,
+                <br className="md:hidden block" /> contribute & earn IOU
+                <br className="md:hidden block" /> tokens!
+              </h2>
+              <button className="banner-btn contained-effect flex items-center bg-gredient-2 hidden md:flex justify-between">
+                <span className="fs-16 font-black mr-3">Begin Quests</span>
+                <ImArrowRight2 />
+              </button>
             </div>
           </div>
         </Slider>

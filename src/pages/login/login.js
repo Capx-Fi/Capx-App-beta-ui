7ff371dbfad055edac1ae214a5f77b3ffe906781
parvez-capx <115526513+parvez-capx@users.noreply.github.com>
2023-02-22 11:59:39 +0530

    onboarding screen copy right year changed

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -137,7 +137,7 @@ const Login = () => {
             <img className=" mt-1" src={ChipCapxSvg} alt="capx" />
           </div>
           <p className="text-gray-400 fs-15 font-bold hidden md:block absolute bottom-0 py-5">
-            © Capx 2022. All rights reserved
+            © Capx 2023. All rights reserved
           </p>
         </div>
       </div>
